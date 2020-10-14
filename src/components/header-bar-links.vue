36d7b3ef967f345284421d0badac3758722478c3
Lautaro <lautaropoletto@gmail.com>
2020-10-14 15:19:32 -0300

    interpolacion de datos, eventos, enlace de propiedades, v-model

---
+++ src/components/header-bar-links.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="navbar-menu">
+    <div class="navbar-end">
+      <div class="navbar-item">
+        <div class="buttons">
+          <a
+            v-bind:href="gitHub"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <i class="fab fa-github fa-2x" aria-hidden="true"></i>
+          </a>
+          <a
+            v-bind:href="twitter"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <i class="fab fa-twitter fa-2x" aria-hidden="true"></i>
+          </a>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<!-- Modelo -->
+<script>
+export default {
+  name: 'HeaderBarLinks',
+  data() {
+    return {
+      gitHub: 'https://github.com/johnpapa/vue-getting-started',
+      twitter: 'https://twitter.com/john_papa'
+    }
+  },
+}
+</script>
---
+++ src/components/heroes.vue
@@ -0,0 +1,133 @@
+<!-- Plantilla -->
+<template>
+  <div class="content-container">
+    <div class="section content-title-group">
+      <h2 class="title">Heroes</h2>
+    </div>
+    <div class="columns">
+      <div class="column is-8">
+        <div class="card edit-detail">
+          <header class="card-header">
+            <p class="card-header-title">{{ hero.firstName }}</p>
+          </header>
+          <div class="card-content">
+            <div class="content">
+              <div class="field">
+                <label class="label" for="id">id</label>
+                <label class="input" id="id" readonly>{{ hero.id }}</label>
+              </div>
+              <div class="field">
+                <label class="label" for="firstName">first name</label>
+                <!-- v-model para una relacion bidireccional -->
+                <input class="input" id="firstName" v-model="hero.firstName"/>
+              </div>
+              <div class="field">
+                <label class="label" for="lastName">last name</label>
+                <input class="input" id="lastName" v-model="hero.lastName"/>
+              </div>
+              <div class="field">
+                <label class="label" for="description">description</label>
+                <textarea class="input" id="description" type="text" v-model="hero.description"/>
+              </div>
+              <div class="field">
+                <label class="label">cape color</label>
+                <label class="radio" for="color-red">
+                  <input type="radio" id="color-red" value="red" v-model="hero.capeColor"/>
+                  red
+                </label>
+                <label class="radio" for="color-blue">
+                  <input type="radio" id="color-blue" value="blue" v-model="hero.capeColor"/>
+                  blue
+                </label>
+                <label class="radio" for="color-green">
+                  <input type="radio" id="color-green" value="green" v-model="hero.capeColor"/>
+                  green
+                </label>
+                <!-- Elegimos el color-->
+                <div class="color-line" :style="{ 'background-color' : hero.capeColor }"></div>
+              </div>
+              <div class="field">
+                <label for="power">
+                  super power
+                  <div class="select is-primary">
+                    <!-- Selects y clases enlazadas -->
+                    <!-- Presionando la tecla escape llamo a la funcion: @keyup.esc="clearPower" -->
+                    <select id="power" 
+                      v-model="hero.power"
+                      :class="{ invalid: !hero.power }"
+                      @keyup.esc="clearPower"
+                    >
+                      <option disabled value>Please select one</option>
+                      <option>Speed</option>
+                      <option>Flight</option>
+                      <option>Strength</option>
+                      <option>Invisibility</option>
+                    </select>
+                  </div>
+                </label>
+              </div>
+              <div class="field">
+                <label class="checkbox" for="active">
+                  active
+                  <!-- propiedad avtivo en el check -->
+                  <input type="checkbox" class="is-primary" id="active"  v-model="hero.active"/>
+                </label>
+              </div>
+            </div>
+          </div>
+          <footer class="card-footer">
+            <!-- capturando eventos -->
+            <!-- v-on:evento="nombre-metodo"-->
+            <!-- Un atajo saria: @evento="nombre-metodo"-->
+            <button class="link card-footer-item cancel-button" v-on:click="cancelHero">
+              <i class="fas fa-undo"></i>
+              <span>Cancel</span>
+            </button>
+            <!-- Si el metodo no recibe parametros se deja sin porentesis -->
+            <!-- de lo contrario colocarlo v-on:click="saveHero(param1, param2)" -->
+            <button class="link card-footer-item" v-on:click="saveHero">
+              <i class="fas fa-save"></i>
+              <span>Save</span>
+            </button>
+          </footer>
+        </div>
+        <div class="notification is-info"><pre>{{ message }}</pre></div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<!-- Modelo -->
+<script>
+export default {
+  name: 'Heroes',
+  // datos del modelo
+  data() {
+    return {
+      hero: {
+        id: 20,
+        firstName: 'Lautaro',
+        lastName: 'Poletto',
+        description: 'description',
+        capeColor: '',
+        power: '',
+        active: true
+      },
+      message: '',
+    };
+  },
+
+  // Metodos
+  methods: {
+    cancelHero() {
+      this.message = '';
+    },
+    clearPower() {
+      this.hero.power = '';
+    },
+    saveHero() {
+      this.message = JSON.stringify(this.hero, null, '\n');
+    }
+  },
+};
+</script>
